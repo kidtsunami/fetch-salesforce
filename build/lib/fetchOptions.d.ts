dc40abdb459f06d7832ffef7fc69d682f691d77b
David Helmer <dhelmer@mavens.com>
2016-11-07 13:19:25 -0500

    Remove old fetchOptions

--- build/lib/fetchOptions.d.ts
+++
@@ -1,6 +0,0 @@
-export interface FetchOptions {
-    headers?: any;
-    method: string;
-    cache?: boolean;
-    body?: any;
-}
--- build/lib/fetchOptions.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=fetchOptions.js.map
