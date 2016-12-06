325938d21adae985941cb836d2b47aadf6721507
David Helmer <dhelmer@mavens.com>
2016-12-06 17:12:01 -0500

    Tests refreshing the token

--- src/__tests__/lib/fetcher.ts
+++ src/__tests__/lib/fetcher.ts
@@ -1,7 +1,20 @@
+import sinon = require('sinon');
 import { withRequiredSalesforceOptions } from './salesforceOptions';
+import fetchMock = require('fetch-mock');
 
 import { Fetcher } from '../../lib/fetcher';
 import { SalesforceOptions } from '../../lib/salesforceOptions';
+let fetch = require('isomorphic-fetch');
+
+
+let refreshAccessTokenResponse = {
+    json: () =>  {
+        return {
+            access_token: 'refreshedAccessToken'
+        };
+    }
+}
+fetchMock.mock('https://instanceURL/requiredtest/services/oauth2/token', refreshAccessTokenResponse);
 
 describe('fetcher', () => {
     let options: SalesforceOptions = withRequiredSalesforceOptions();
@@ -40,8 +53,21 @@ describe('fetcher', () => {
             fetcher = Fetcher.Create(options);
         });
 
+        afterEach(() => {
+        });
+
         it('returns accessToken if there', () => {
             let expectedAccessToken = 'existingAccessToken';
+            fetcher.options.accessToken = expectedAccessToken;
+
+            return fetcher.getAccessToken()
+                .then((actualAccessToken) => {
+                    expect(actualAccessToken).toEqual(expectedAccessToken);
+                });
+        });
+
+        it('refreshes accessToken if not there', () => {
+            let expectedAccessToken = 'refreshedAccessToken';
             fetcher.options.accessToken = expectedAccessToken;
 
             return fetcher.getAccessToken()
